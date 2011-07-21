45cbda40fc7af979cc7a0c2924780e55bdfd7e73
gustavofonseca <gustavofons@gmail.com>
2011-07-21 14:43:55 -0300

    preparing to host project pages

--- couchapp/views/monographs_and_parts/map.js
+++
@@ -1,8 +0,0 @@
-function(doc){
-    if(doc.TYPE == 'Monograph'){
-        emit([doc._id, 0], null);
-    }
-    if(doc.TYPE == 'Part'){
-        emit([doc.monograph, 1], null);
-    }
-}
--- scielobooks/resources.py
+++
@@ -1,9 +0,0 @@
-from pyramid.security import Allow, Everyone
-
-class RootFactory(object):
-    __acl__ = [ (Allow, Everyone, 'view'),
-                (Allow, 'editor', 'editors'),
-                (Allow, 'admin', ('editors','admin')),
-              ]
-    def __init__(self, request):
-        pass
